5dd30120b2cc26aef77580a0fb0f4f94f901171a
Kaustav Debnath <kaustav.sfdc@gmail.com>
2025-06-18 11:30:11 +0530

    improved hangman

--- 100 Days of Python/Day 7 - Hangman/Hangman.py
+++ 100 Days of Python/Day 7 - Hangman/Hangman.py
@@ -71,6 +71,15 @@ def draw_hangman(guesses):
        
     ]
     return stages[guesses]
+def return_guessed_word(word_to_guess, guessed_list):
+    placeholder = ""
+    for letter in word_to_guess:
+        if letter in guessed_list:
+            placeholder += letter
+        else:
+            placeholder += "_"
+    # print('placeholder==>',placeholder)
+    return placeholder
 
 while wrong_guesses < 6 and guessed_word != word_to_guess:
 
@@ -80,18 +89,22 @@ while wrong_guesses < 6 and guessed_word != word_to_guess:
     if guess in word_to_guess:
         print(f"Your guessed letter '{guess}' is in the word.")
         guessed_letters.append(guess)
-        guessed_word = "".join([letter if letter in guessed_letters else "_" for letter in word_to_guess])  
+        # guessed_word = "".join([letter if letter in guessed_letters else "_" for letter in word_to_guess])  
+        guessed_word = return_guessed_word(word_to_guess,guessed_letters)
     else:
         print(f"Sorry, the letter '{guess}' is not in the word.")
         
         wrong_guessed_letters.append(guess)
         wrong_guesses += 1
-        print(f"Wrongly Guessed letters: {wrong_guessed_letters}")
+        print(f"Wrongly Guessed letters: {wrong_guessed_letters} and you have {6 - wrong_guesses} attempts left!")
         print(draw_hangman(wrong_guesses))
     if guessed_word == "":
         print("You haven't guessed any letters yet.")
     elif guessed_word == word_to_guess:
-        print(f"\nCongratulations! You've guessed the word: {word_to_guess}")
+        print(f"\nCongratulations! You've guessed the word: {word_to_guess.upper()}")
+        break
+    elif wrong_guesses == 6:
+        print(f"\nSorry you have lost the game! The correct word was {word_to_guess.upper()}")
         break
     else:
         print(f"\nWord guessed so far: {guessed_word}")
