6ad19282de9782480f4252ed5a019d85a87e84e0
Kaustav Debnath <kaustav.sfdc@gmail.com>
2025-07-03 11:43:01 +0530

    small changes to the game

--- 100 Days of Python/Day 14 - Higher Lower Game Project/main.py
+++ 100 Days of Python/Day 14 - Higher Lower Game Project/main.py
@@ -1,7 +1,7 @@
 from art import logo, vs
 from game_data import data
 import random
-print(logo)
+
 
 USER_SCORE = 0
 DID_USER_WIN = True
@@ -48,18 +48,21 @@ def play_game():
     """
     global USER_SCORE
     global DID_USER_WIN
+    
     random_choice_a = fetch_game_data(data,{})
-
+    
     while DID_USER_WIN:
+        print(logo)
         random_choice_b = fetch_game_data(data, random_choice_a)
         print(f"Compare A: {random_choice_a['name']},a {random_choice_a['description']}, from {random_choice_a['country']}")
         print(vs)
         print(f"Compare B: {random_choice_b['name']},a {random_choice_b['description']}, from {random_choice_b['country']}")
-        user_choice = input("Who has more followers? Type 'A' or 'B': ").strip().upper()
+        user_choice = input("Who has more followers? Type 'A' or 'B': ").starip().upper()
         winning_user = compare_users(random_choice_a, random_choice_b)
         DID_USER_WIN = check_user_response(user_choice, random_choice_a, random_choice_b, winning_user)
         if DID_USER_WIN:
             print(f"You Win! Current Score: {USER_SCORE}")
             random_choice_a = random_choice_b
     print(f"You Lose!: Your Final Score: {USER_SCORE}")
+    print("\n"*20)
 play_game()
